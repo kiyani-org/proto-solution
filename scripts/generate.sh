33f0f91bdf717b61e1ca58e0f84b1f3e45d24c93
Shan Kiyani <shankiyani@google.com>
2023-09-09 16:59:37 -0400

    script refactoring

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
-# Get the current directory
-current_dir=$1
-
-# Get the name of the new directory
-new_dir_name=$2
-
-# Name of the folder where raw protos exist
-protos_prefix=protos/
-
-# Create the new directory if it doesn't exist
-if [ ! -d $new_dir_name ]; then
-  mkdir -p $new_dir_name
-fi
-
-# Check if the go.mod exists
-if [ ! -f "$new_dir_name/go.mod" ]; then
-  # Create the file
-  echo "init go module"
-  cd "$new_dir_name"
-  go mod init github.com/kiyani-org/proto-solution
-  cd ../..
-fi
-
 # Recursively loop through the current directory and its subdirectories
-function copy_files() {
+function generate_protos_rec() {
   for file_or_dir in "$1"/*; do
     echo "entry $file_or_dir"
-    trimmed_path=${file_or_dir#$protos_prefix}
+    trimmed_path=${file_or_dir#$raw_protos_dir/}
 
     if [ -f "$file_or_dir" ]; then
       echo "trimmed $trimmed_path"
 
       # Copy the file_or_dir to the new directory
-      echo "copying ($file_or_dir) to ($new_dir_name/$trimmed_path)"
-      # cp "$file_or_dir" "$new_dir_name/$trimmed_path"
-      export PATH="$PATH:$(go env GOPATH)/bin" && protoc --go_out=$new_dir_name $file_or_dir
+      echo "copying ($file_or_dir) to ($dest_root_dir/$trimmed_path)"
+      export PATH="$PATH:$(go env GOPATH)/bin" && protoc --go_out=$dest_root_dir $file_or_dir
     elif [ -d "$file_or_dir" ]; then
-      echo "mkdir ($new_dir_name/$trimmed_path)"
-
-      mkdir "$new_dir_name/$trimmed_path"
+      echo "mkdir ($dest_root_dir/$trimmed_path)"
       # Recursively copy the contents of the subdirectory
-      copy_files "$file_or_dir"
+      generate_protos_rec "$file_or_dir"
     fi
   done
 }
 
-# Copy the files from the current directory to the new directory
-copy_files $current_dir
+function pre_gen_go {
+  # Create the new directory if it doesn't exist
+  if [ ! -d $1 ]; then
+    mkdir -p $1
+  fi
 
-cd gen/go
-go mod tidy
+  # Check if the go.mod exists
+  if [ ! -f "$1/go.mod" ]; then
+    # Create the file
+    echo "init go module"
+    cd $1
+    go mod init github.com/kiyani-org/proto-solution
+    cd -
+  fi
+}
+
+function post_gen_go() {
+  cd $1
+  go mod tidy
+  cd -
+}
+
+function main() {
+  # go protos
+  dest_root_dir=gen/go
+  pre_gen_go $dest_root_dir
+  generate_protos_rec $raw_protos_dir
+  post_gen_go $dest_root_dir
+}
+
+# Get the current directory
+raw_protos_dir=protos
+
+main 
