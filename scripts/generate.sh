cd027674d21e6d193e1edffa7582e319de62c510
Shan Kiyani <shankiyani@google.com>
2024-02-29 14:10:33 -0800

    upload to node gar (#18)
    * upload to node gar
    
    * more stuff
    
    * fix version
    
    * testing how to pull variable from sep wkflow
    
    * support node upload artifact
    
    * upload workflow change

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -23,8 +23,45 @@ function post_gen_go {
   cd -
 }
 
+function pre_gen_node {
+  echo "running pre_gen_node"
+
+  # Create the new directory if it doesn't exist
+  if [ ! -d $1 ]; then
+    mkdir -p $1
+  fi
+
+  # Configure package manager to use with npm
+  cat > $1/.npmrc << EOF
+@protos:registry=https://us-npm.pkg.dev/shankiyani-dev-95/proto-node/
+//us-npm.pkg.dev/shankiyani-dev-95/proto-node/:always-auth=true
+EOF
+  
+  # Create the package.json with appropriate version -- same version can never be published twice
+  cat > $1/package.json << EOF
+{ 
+  "name": "@protos/proto-node",
+  "version": "$PROTO_VERSION",
+  "main": "index.ts",
+  "scripts": {
+    "artifactregistry-login": "npx google-artifactregistry-auth --repo-config=\".npmrc\" --credential-config=\".npmrc\""
+  }
+}
+EOF
+
+  touch $1/index.ts
+}
+
+function post_gen_node {
+  echo "running post_gen_node"
+  cd $1
+  npm i
+  cd -
+}
+
 function main {
   pre_gen_go gen/go
+  pre_gen_node gen/node
 
   buf generate
   if [ $? -ne 0 ]; then
@@ -33,6 +70,7 @@ function main {
   fi
 
   post_gen_go gen/go
+  post_gen_node gen/node
 }
 
 main 
