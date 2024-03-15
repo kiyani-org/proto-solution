09a7d4b1d2fc30df393e8bfd291cc8fb4120a599
Shan Kiyani <shankiyani@google.com>
2024-03-14 23:50:36 -0400

    testing passing env (#26)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 function pre_gen_go {
   echo "running pre_gen_go"
@@ -30,10 +31,7 @@ function post_gen_go {
 function pre_gen_node {
   echo "running pre_gen_node"
 
-  proto_version_key='proto_version:'
-  proto_version_file='version.yaml'
-
-  PROTO_VERSION=$(grep $proto_version_key $proto_version_file | cut -c $((${#proto_version_key} + 1))- | xargs)
+  PROTO_VERSION=${PROTO_VERSION:-$(grep 'proto_version:' 'version.yaml' | cut -d':' -f2 | xargs echo)}
 
   # Create the new directory if it doesn't exist
   if [ ! -d $1 ]; then
